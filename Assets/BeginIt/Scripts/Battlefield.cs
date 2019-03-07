ca64cf662171d2351881d8985ba0d01bdf42579b
S901059999 <s901052002@yahoo.com.tw>
2019-03-07 15:18:53 +0900

    Changed ObjectEx methods.

--- Assets/BeginIt/Scripts/Battlefield.cs
+++ Assets/BeginIt/Scripts/Battlefield.cs
@@ -14,7 +14,7 @@ namespace BeginIt.Quadrone {
             for (int x = 0; x < WIDTH; x++) {
                 for (int y = 0; y < HEIGHT; y++) {
                     areas.Add(new Vector2(x, y),
-                              ObjectEx.Instantiate<Area>(areaPrefab, new Vector2((x - 2.5f) * 2.25f, y * -1.5f), transform)
+                              ObjectEx.Instantiate<Area>(areaPrefab, transform, new Vector2((x - 2.5f) * 2.25f, y * -1.5f))
                                       .Creat((Team)(x / (WIDTH / 2))));
                 }
             }
--- Assets/BeginIt/Scripts/Common/ObjectEx.cs
+++ Assets/BeginIt/Scripts/Common/ObjectEx.cs
@@ -8,6 +8,13 @@ namespace BeginIt {
         }
 
         public static T Instantiate<T>(Object original, Vector3 position, Transform parent) where T : Object {
+            return Object.Instantiate(original as T, position, Quaternion.identity, parent);
+        }
+
+        /// <summary>
+        /// Set position from parent's position.
+        /// </summary>
+        public static T Instantiate<T>(Object original, Transform parent, Vector3 position) where T : Object {
             return Object.Instantiate(original as T, parent.position + position, Quaternion.identity, parent);
         }
     }
