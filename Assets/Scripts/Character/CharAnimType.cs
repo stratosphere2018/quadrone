18aff88165d6ac9b52ca19ff87e544f6f190d800
shushu1105 <48203402+shushu1105@users.noreply.github.com>
2019-03-14 21:14:32 +0900

    Merge branch 'develop' of https://github.com/stratosphere2018/quadrone into develop
    # Conflicts:
    #	Assets/Scenes/Home.unity
    #	Assets/Scripts/Character/CharAnimType.cs
    #	Assets/TextMesh Pro/Resources/Fonts & Materials/LiberationSans SDF.asset

--- Assets/Scripts/Character/CharAnimType.cs
+++ Assets/Scripts/Character/CharAnimType.cs
@@ -2,7 +2,14 @@
 {
     public enum CharAnimType
     {
+<<<<<<< HEAD
+        Buster,
         Canon,
         Sword,
+        Vulcan,
+=======
+        Canon,
+        Sword,
+>>>>>>> 9e66ef548c32b64dfd9e9533737eeb7251eda0d1
     }
 }
---
+++ Assets/SelectText.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Stratosphere.Quadrone
+{
+    [RequireComponent(typeof(TextMesh))]
+    public class SelectText : MonoBehaviour
+    {
+        public string[] SceneName;
+        public string[] ShowText;
+        private TextMesh Mesh;
+        private int Select;
+        bool Error = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Mesh = GetComponent<TextMesh>();
+            Select = 0;
+
+            if (SceneName.Length != ShowText.Length)
+            {
+                Debug.LogError("シーン数と項目数が一致してない");
+                Error = true;
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Error) return;
+
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                Select++;
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                Select--;
+            }
+            Mathf.Clamp(Select, 0, 3);
+
+            Mesh.text = "";
+            for (int i = 0; i < ShowText.Length; i++)
+            {
+                if (i == Select)
+                {
+                    Mesh.text += "ﾆｱ ";
+                }
+                else
+                {
+                    Mesh.text += "   ";
+                }
+                Mesh.text += ShowText[i] + "\n";
+            }
+
+
+        }
+    }
+}
