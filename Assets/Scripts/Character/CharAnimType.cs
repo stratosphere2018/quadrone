1c02944b4b5f69339d0fae89a6056898b3f38f40
o0-kettle-0o <43896951+o0-kettle-0o@users.noreply.github.com>
2019-03-14 21:23:18 +0900

    アニメーションタイプを編集

--- Assets/Scripts/Character/CharAnimType.cs
+++ Assets/Scripts/Character/CharAnimType.cs
@@ -2,14 +2,13 @@
 {
     public enum CharAnimType
     {
-<<<<<<< HEAD
-        Buster,
-        Canon,
-        Sword,
-        Vulcan,
-=======
-        Canon,
-        Sword,
->>>>>>> 9e66ef548c32b64dfd9e9533737eeb7251eda0d1
+        Normal,
+        Charge,
+        Shoot,
+        Slash,
+        Throw,
+        Canon = Shoot,
+        Sword = Slash,
+        Vulcan = Shoot,
     }
 }
