db9e68d2599e77c2d063955b6809460b678db653
o0-kettle-0o <43896951+o0-kettle-0o@users.noreply.github.com>
2019-03-15 22:07:49 +0900

    文字列ペアをインスペクタ―で操作できるようにStringPair型作成

---
+++ Assets/Scripts/Stratosphere/StringPair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stratosphere
+{
+    /// <summary>
+    /// 文字列ペア
+    /// </summary>
+    [Serializable]
+    public class StringPair
+    {
+        /// <summary>
+        /// キーとなる値
+        /// </summary>
+        public string key;
+        /// <summary>
+        /// 値
+        /// </summary>
+        public string value;
+    }
+}
