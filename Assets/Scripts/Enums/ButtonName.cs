6ab57ed7d3efbd735f7945d0eef96b4287765d8d
o0-kettle-0o <43896951+o0-kettle-0o@users.noreply.github.com>
2019-03-17 22:39:00 +0900

    コメントを追加

--- Assets/Scripts/Enums/ButtonName.cs
+++ Assets/Scripts/Enums/ButtonName.cs
@@ -1,11 +1,23 @@
 ﻿namespace Stratosphere.Quadrone
 {
-	public enum ButtonName
-	{
+    public enum ButtonName
+    {
+        /// <summary>
+        /// X軸
+        /// </summary>
 		Horizontal,
+        /// <summary>
+        /// Y軸
+        /// </summary>
 		Vertical,
+        /// <summary>
+        /// 決定(エンター、スペース、ボタン0)
+        /// </summary>
 		Submit,
+        /// <summary>
+        /// キャンセル(Esc、ボタン1)
+        /// </summary>
 		Cancel,
-	}
+    }
 }
 
