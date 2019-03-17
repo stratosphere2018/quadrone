c675c2de837c0a21291343b908139c405510faf6
o0-kettle-0o <43896951+o0-kettle-0o@users.noreply.github.com>
2019-03-17 22:37:02 +0900

    コンストラクタを追加

--- Assets/Scripts/Stratosphere/Pair.cs
+++ Assets/Scripts/Stratosphere/Pair.cs
@@ -2,9 +2,19 @@
 
 namespace Stratosphere
 {
-	public class Pair<TKey, TValue>
-	{
-		public TKey key;
-		public TValue value;
-	}
+    public class Pair<TKey, TValue>
+    {
+        public TKey key;
+        public TValue value;
+
+        public Pair()
+        {
+        }
+
+        public Pair(TKey key, TValue value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
