de3981cdc0fcbcf5fe2918a56c2f23f4d690bdb7
o0-kettle-0o <43896951+o0-kettle-0o@users.noreply.github.com>
2019-03-18 21:32:49 +0900

    インターネット接続テスト

---
+++ Assets/Scripts/SocketClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+namespace Stratosphere.Quadrone
+{
+    public class SocketClient : MonoBehaviour
+    {
+        private WebSocket ws;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ws = new WebSocket(Secret.WSS);
+            ws.OnMessage += (sender, e) =>
+            {
+                Debug.Log(e.Data);
+            };
+            ws.Connect();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (InputExt.GetButton00Press())
+            {
+                ws.Send("test");
+            }
+        }
+    }
+}
